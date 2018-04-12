50e712443cc11297c5d852293e514c06c96fcac0
Diego Casati <diego.casati@gmail.com>
2018-04-12 10:43:26 -0700

    fixes some woes with Kubernetes secrets and volumes. No matter what I try, it k8s keeps mounting the secrets volume as 777. looks like this is a bug

--- kubernetes/ubuntu/dockerfiles/entrypoint.sh
+++ kubernetes/ubuntu/dockerfiles/entrypoint.sh
@@ -3,6 +3,8 @@
 mkdir -p {/var/run/sshd,/root/.ssh/,/etc/yubikey} \
     && touch /root/.ssh/authorized_keys
 
+chmod 0744 /root/.ssh/
+
 CLIENT_ID=$(cat /etc/yubikey/client_id)
 YUBI_SECRET=$(cat /etc/yubikey/secretkey)
 
@@ -10,10 +12,10 @@ sed -i \
     "1s!^!auth sufficient pam_yubico.so id=${CLIENT_ID} key=${YUBI_SECRET} authfile=/etc/yubikey/yubikey_mappings!" /etc/pam.d/sshd
 
 sed -i \
-    's/PermitRootLogin prohibit-password/PermitRootLogin yes/;  \
+    "s/PermitRootLogin prohibit-password/PermitRootLogin yes/;  \
     s/^#*PasswordAuthentication .*/PasswordAuthentication no/; \
     s/^#*ChallengeResponseAuthentication .*/ChallengeResponseAuthentication yes/;  \
     s/^#*IgnoreRhosts.*/AuthenticationMethods publickey,keyboard-interactive:pam/; \
-    s/^#*UsePAM .*/UsePAM yes/' /etc/ssh/sshd_config
+    s/^#*UsePAM .*/UsePAM yes/" /etc/ssh/sshd_config
 
 /usr/sbin/sshd -D
