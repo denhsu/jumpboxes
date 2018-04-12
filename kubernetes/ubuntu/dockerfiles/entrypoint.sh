b6c5ac8072879b3f41cd00665cd572bcacc0eb75
Diego Casati <diego.casati@gmail.com>
2018-04-12 14:37:01 -0700

    add entries that deal with the ssh keys. without them the CentOS image would no boot

--- kubernetes/ubuntu/dockerfiles/entrypoint.sh
+++ kubernetes/ubuntu/dockerfiles/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
+ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
+
 mkdir -p {/var/run/sshd,/root/.ssh/,/etc/yubikey} \
     && touch /root/.ssh/authorized_keys
 
